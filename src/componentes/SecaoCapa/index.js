5007a0c64532014bf704c7bef20012e7bbc01d20
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-10-29 12:08:40 -0300

    feat: Adicionar componente SecaoCapa

---
+++ src/componentes/SecaoCapa/index.js
@@ -0,0 +1,11 @@
+export default function SecaoCapa() {
+    return (
+        <section>
+            <div>
+                <p>Preços baixos em</p>
+                <h1>ÓCULOS DE GRAU E DE SOL</h1>
+                <p>Você só encontra aqui</p>
+            </div>
+        </section>
+    )
+}
