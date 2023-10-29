f5504b282435f92eaa03a1952d7ea53723686d03
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-10-29 12:09:28 -0300

    feat: Adicionar componente SecaoContato

---
+++ src/componentes/SecaoContato/index.js
@@ -0,0 +1,42 @@
+export default function SecaoContato() {
+    return (
+        <section id="contato">
+            <div>
+                <h2>FALE CONOSCO</h2>
+                <p>Não perca tempo, venha conhecer uma de nossas lojas ou entre em contato através de nossas redes sociais ou da central de atendimento.</p>
+                <div>
+                    <div>
+                        <h3>Contato</h3>
+                        <figure>
+                            <img src="assets/local.png" alt="Ícone de Endereço" title="Ícone de Endereço"/>
+                            <figcaption>Nova Iguaçu, RJ</figcaption>
+                        </figure>
+                        <figure>
+                            <img src="assets/telefone.png" alt="Ícone de Telefone" title="Ícone de Telefone"/>
+                            <figcaption>(21) 9999-9999</figcaption>
+                        </figure>
+                        <figure>
+                            <img src="assets/email.png" alt="Ícone de E-mail" title="Ícone de E-mail"/>
+                            <figcaption>contato@oticavida.com</figcaption>
+                        </figure>
+                    </div>
+                    <div>
+                    <h3>Nossas Redes Sociais</h3>
+                        <figure>
+                            <img src="assets/fb.png" alt="Ícone do Facebook" title="Ícone do Facebook"/>
+                            <figcaption>/OticaVida</figcaption>
+                        </figure>
+                        <figure>
+                            <img src="assets/ig.png" alt="Ícone do Instagram" title="Ícone do Instagram"/>
+                            <figcaption>@oticavidarj</figcaption>
+                        </figure>
+                        <figure>
+                            <img src="assets/tt.png" alt="Ícone do " title="Ícone do Twitter"/>
+                            <figcaption>@oticavidarj</figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
