6a8be3a91839a27ed2fcd356917e19cc9abd68df
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-10-29 12:08:10 -0300

    feat: Adicionar componente Topo

---
+++ src/componentes/Topo/index.js
@@ -0,0 +1,15 @@
+export default function Topo() {
+    return (
+        <header>
+            <div>
+                <img src="assets/logo.png" alt="Logo Óticas Vida"></img>
+
+                <nav>
+                    <a href="#produtos">PRODUTOS</a>
+                    <a href="#sobre">SOBRE</a>
+                    <a href="#contato">CONTATO</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
