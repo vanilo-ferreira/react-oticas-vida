cffb828176b38aec6930d2afd85ab8d64fc6fabe
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-10-29 12:08:52 -0300

    feat: Adicionar componente SecaoProdutos

---
+++ src/componentes/SecaoProdutos/index.js
@@ -0,0 +1,40 @@
+export default function SecaoProdutos() {
+    return (
+        <section id="produtos">
+            <div>
+                <h2>NOSSOS PRODUTOS</h2>
+                <p>Trabalhamos com óculos de grau, óculos de sol, lentes transition nos modelos masculino, feminino e infantil.</p>
+                <p>Todos os nossos preços são acessíveis e contam com a melhor qualidade de mercado.</p>
+                <div>
+                    <div>
+                        <h3>Óculos de grau</h3>
+                        <img src="assets/oculos01.png" title='Óculos de grau' alt='Armação de óculos de grau' />
+                        <p>R$ 500,00</p>
+                    </div>
+                    <div>
+                        <h3>Óculos transition</h3>
+                        <img src="assets/oculos02.png" title='Óculos transition' alt='Armação de óculos transition' />
+                        <p>R$ 750,00</p>
+                    </div>
+                    <div>
+                        <h3>Óculos de sol</h3>
+                        <img src="assets/oculos03.png" title='Óculos de sol' alt='Armação de óculos de sol' />
+                        <p>R$ 700,00</p>
+                    </div>
+                    <div>
+                        <h3>Óculos infantil</h3>
+                        <img src="assets/oculos04.png" title='Óculos infantil' alt='Armação de óculos infantil' />
+                        <p>R$ 500,00</p>
+                    </div>
+                </div>
+                <p>Todos os nossos produtos incluem:</p>
+                <ul>
+                    <li>Garantia de 1 ano</li>
+                    <li>Manutenção preventiva</li>
+                    <li>Descontos especiais na compra da segunda unidade</li>
+                    <li>Flexibilidade de pagamento</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
