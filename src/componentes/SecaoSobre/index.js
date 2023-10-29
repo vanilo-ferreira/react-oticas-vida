aacc7e834f8d18f4ff83621a7ddde9778d26ee2e
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-10-29 12:09:13 -0300

    feat: Adicionar componente SecaoSobre

---
+++ src/componentes/SecaoSobre/index.js
@@ -0,0 +1,22 @@
+export default function SecaoSobre() {
+    return (
+        <section id="sobre">
+            <div>
+                <h2>QUEM SOMOS NÓS?</h2>
+                <p>Fundada em 2001, em Nova iguaçu - Rio de Janeiro, a òticas vida iniciou suas atividades focada no atendimento ao público de renda mais baixa, sempre com o objetivco de proporcionar ao cliente bom atendimento, qualidade e preço baixo.</p>
+                <div>
+                <img src="assets/loja.png" title="Imagem da loja" alt="Imagem de prateleira de remédios da Óticas vida"/>
+                    <div>
+                        <h3>NOSSAS FILIAIS</h3>
+                        <p>Hoje temos mais de 20 filiais pelo Brasil e na América</p>
+                    </div> 
+                    <div>
+                        <h3>ATENDIMENTO FLEXIVEL</h3>
+                        <p>Nossa equipe possui treinamentos para atender</p>
+                    </div>
+                    <img src="assets/atendimento.png" title="Atendimento" alt="Funcionária realizando um atendimento na Óticas vida"></img>
+                </div>
+            </div>
+        </section>
+    )
+}
