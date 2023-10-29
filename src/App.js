8e2457dc647bf8074ac66b5dfd8d63553652c262
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-10-29 12:11:13 -0300

    feat: Adicionar componentes Topo, Conteudo e Rodape a aplicação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
+import Topo from './componentes/Topo/';
+import Conteudo from './componentes/Conteudo/';
+import Rodape from './componentes/Rodape/';
+
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Topo />
+      <Conteudo />
+      <Rodape />
     </div>
   );
 }
