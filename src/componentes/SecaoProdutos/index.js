7cf1f03c3dfaca7821b0b332accce106d9d2e08a
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-10-30 22:56:37 -0300

    feat: Estilizar o componente SecaoProduto

--- src/componentes/SecaoProdutos/index.js
+++ src/componentes/SecaoProdutos/index.js
@@ -1,34 +1,36 @@
+import './style.css';
+
 export default function SecaoProdutos() {
     return (
-        <section id="produtos">
-            <div>
-                <h2>NOSSOS PRODUTOS</h2>
+        <section id="produtos" className="secao-produtos">
+            <div className="limitar-secao">
+                <h2 className="texto-subtitulo">NOSSOS PRODUTOS</h2>
                 <p>Trabalhamos com óculos de grau, óculos de sol, lentes transition nos modelos masculino, feminino e infantil.</p>
                 <p>Todos os nossos preços são acessíveis e contam com a melhor qualidade de mercado.</p>
-                <div>
-                    <div>
+                <div className="container-produtos">
+                    <div className='produtos-card'>
                         <h3>Óculos de grau</h3>
                         <img src="assets/oculos01.png" title='Óculos de grau' alt='Armação de óculos de grau' />
                         <p>R$ 500,00</p>
                     </div>
-                    <div>
+                    <div className='produtos-card'>
                         <h3>Óculos transition</h3>
                         <img src="assets/oculos02.png" title='Óculos transition' alt='Armação de óculos transition' />
                         <p>R$ 750,00</p>
                     </div>
-                    <div>
+                    <div className='produtos-card'>
                         <h3>Óculos de sol</h3>
                         <img src="assets/oculos03.png" title='Óculos de sol' alt='Armação de óculos de sol' />
                         <p>R$ 700,00</p>
                     </div>
-                    <div>
+                    <div className='produtos-card'>
                         <h3>Óculos infantil</h3>
                         <img src="assets/oculos04.png" title='Óculos infantil' alt='Armação de óculos infantil' />
                         <p>R$ 500,00</p>
                     </div>
                 </div>
                 <p>Todos os nossos produtos incluem:</p>
-                <ul>
+                <ul className='lista-produtos'>
                     <li>Garantia de 1 ano</li>
                     <li>Manutenção preventiva</li>
                     <li>Descontos especiais na compra da segunda unidade</li>
