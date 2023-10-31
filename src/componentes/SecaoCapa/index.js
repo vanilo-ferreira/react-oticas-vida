16637630e86837a946b64cc5bb7559e3bd5163a2
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-10-30 22:56:25 -0300

    feat: Estilizar o componente SecaoCapa

--- src/componentes/SecaoCapa/index.js
+++ src/componentes/SecaoCapa/index.js
@@ -1,10 +1,12 @@
+import "./style.css"
+
 export default function SecaoCapa() {
     return (
-        <section>
-            <div>
-                <p>Preços baixos em</p>
-                <h1>ÓCULOS DE GRAU E DE SOL</h1>
-                <p>Você só encontra aqui</p>
+        <section className="secao-capa">
+            <div className="limitar-secao capa texto-capa">
+                <p className="paragrafo-capa">Preços baixos em</p>
+                <h1 className="titulo-capa">Óculos de <br/>grau e de sol</h1>
+                <p className="paragrafo-capa">Você só encontra aqui</p>
             </div>
         </section>
     )
